8e5dcaf083381620609e9301e27a0628c256f8de
Kolosek <office@kolosek.com>
2018-07-11 13:15:53 +0200

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
 class Article < ActiveRecord::Base
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings #da bi mogla direktno da pristupam sa articles.tags
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq #uzme sv e,splituje,prebaci u mala slova i onda uzme samo unique
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
